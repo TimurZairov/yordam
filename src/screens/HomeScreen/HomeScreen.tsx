f8abe2da7a7d4e37813f8623912b9a42627e9e9a
TimurZairov <zairovne@gmail.com>
2023-05-03 19:02:39 +0500

    Svg Icons & react-native-svg & react-native-svg-transform

--- src/screens/HomeScreen/HomeScreen.tsx
+++ src/screens/HomeScreen/HomeScreen.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import {Text, View} from 'react-native';
+import {Logo} from '../../assets/icons/index';
+
+const pressed = () => {
+  console.warn('pressed');
+};
 
 const HomeScreen = () => {
   return (
     <View>
       <Text>Home</Text>
+      <Logo width={65} height={65} fill={'#6E64BD'} onPress={pressed} />
     </View>
   );
 };
---
+++ src/theme/Colors.ts
@@ -0,0 +1,10 @@
+export const Colors = {
+  backgroundColor: '#F7F7F7',
+  whiteColor: '#FDFDFE',
+  inputColor: '#FBFBFB',
+  whiteFilterColor: '#FBFAFB',
+  blackColor: '#0F0F0F',
+  lightGrayColor: '#DBDBDB',
+  grayColor: '#C8C8C8',
+  darkGrayColor: '#666666',
+};
