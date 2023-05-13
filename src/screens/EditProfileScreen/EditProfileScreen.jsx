207284c96b6576f9f1507f184504c9193e948f14
TimurZairov <zairovne@gmail.com>
2023-05-13 05:12:21 +0500

    EditProfile | ImagePicker | installed after some no need packages installing

--- src/screens/EditProfileScreen/EditProfileScreen.jsx
+++ src/screens/EditProfileScreen/EditProfileScreen.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Image, ScrollView, Text, View} from 'react-native';
 import {useRoute} from '@react-navigation/native';
 import styles from './style';
@@ -8,8 +8,10 @@ import {useForm} from 'react-hook-form';
 import Button from '../../Components/Button';
 import {colors} from '../../theme/colors';
 import ButtonOnPress from '../../Components/Button/ButtonOnPress';
+import {launchImageLibrary} from 'react-native-image-picker';
 
 const EditProfileScreen = () => {
+  const [changeAvatar, setChangeAvatar] = useState(null);
   const router = useRoute();
   const {user} = router.params;
   //Hook form
@@ -22,12 +24,28 @@ const EditProfileScreen = () => {
     },
   });
 
+  const imageHandler = async () => {
+    const result = await launchImageLibrary(
+      {mediaType: 'photo'},
+      ({didCancel, errorCode, errorMessage, assets}) => {
+        if (!didCancel && !errorCode && assets && assets.length) {
+          setChangeAvatar(assets[0]);
+        }
+      },
+    );
+  };
+
   return (
     <ScrollView style={styles.container}>
       <Header />
       <View style={styles.imageContainer}>
-        <Image source={{uri: user.image}} style={styles.image} />
-        <Text style={styles.imageText}>Сменить Фото</Text>
+        <Image
+          source={{uri: changeAvatar?.uri || user.image}}
+          style={styles.image}
+        />
+        <Text style={styles.imageText} onPress={imageHandler}>
+          Сменить Фото
+        </Text>
       </View>
       <Text style={styles.label}> Ваше имя</Text>
       <Input
