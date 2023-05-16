156cfc2c193db006eb4ae2afbb48de424386313f
TimurZairov <zairovne@gmail.com>
2023-05-16 15:50:06 +0500

    Amplify init backend | connect amplify - configure

--- App.tsx
+++ App.tsx
@@ -1,8 +1,11 @@
 import 'react-native-gesture-handler';
 import React from 'react';
 import {enableLatestRenderer} from 'react-native-maps';
-
+import {Amplify} from 'aws-amplify';
+import awsExports from './src/aws-exports';
 import Navigation from './src/Navigation';
+
+Amplify.configure(awsExports);
 
 enableLatestRenderer();
 
